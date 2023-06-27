bafaffe782584cc5b8de3bff17202cd4a4b26c34
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2023-06-27 17:56:47 +0530

    update grid

--- Frontend/src/components/FormComponent/FormComponent.jsx
+++ Frontend/src/components/FormComponent/FormComponent.jsx
@@ -1,53 +1,78 @@
 import Form from "@rjsf/core";
 import React, { useState } from "react";
-import "./FormComponent.css"
+import "./FormComponent.css";
+import axios from "axios";
 
-const FormComponent = ({ jsonData }) => {
-  //const [formData, setformData] = useState([]);
+const FormComponent = ({ jsonData, editData }) => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-  //let schema = localStorage.getItem("jsonSchema");
-  //const schema = require("../jsonFiles/schema.json");
-  // const newData = require("../jsonFiles/searchBox.json");
-  //const schema = jsonData;
-  let schema1 = JSON.parse(localStorage.getItem("jsonSchema"));
-  console.log(schema1);
-  // console.log(schema);
-  const uiSchema = require("../../jsonFiles/uiSchema.json");
-  // const CustomSchemaField = function () {
-  //   return (
-  //     <div id="custom">
-  //       <p>Yeah, I'm pretty dumb.</p>
-  //       {/* <SchemaField {...props} /> */}
-  //     </div>
-  //   );
-  // };
-  // const CustomTitleField = ({ title, required }) => {
-  //   //const legend = required ? title + "****" : title;
-  //   return <div id="custom">legend</div>;
-  // };
-  // const fields = {
-  //   TitleField: CustomTitleField,
-  // };
-  // const handleSubmit = () => {
-  //   console.log(formData);
-  // };
-  return (
-    <div>
-      <div className="form">
-        <Form
-          schema={schema1}
-          //uiSchema={uiSchema}
-          // formData={formData}
-          // onChange={(e) => {
-          //   setformData(e.formData);
-          //   console.log("Hii");
-          //   console.log(formData);
-          // }}
-          //onSubmit={handleSubmit}
-        />
-      </div>
-    </div>
-  );
+        const form = e.target;
+        const formData = new FormData(form);
+
+        const customerId = editData.customerId;
+
+        const requestOptions = {
+            method: "PUT",
+            body: JSON.stringify(Object.fromEntries(formData)),
+        };
+
+        const apiUrl = `https://localhost:7184/api/Customers/Customers/${customerId}`;
+
+        axios
+            .put(apiUrl, requestOptions.body)
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+    //const [formData, setformData] = useState([]);
+
+    //let schema = localStorage.getItem("jsonSchema");
+    //const schema = require("../jsonFiles/schema.json");
+    // const newData = require("../jsonFiles/searchBox.json");
+    //const schema = jsonData;
+    let schema1 = JSON.parse(localStorage.getItem("jsonSchema"));
+    console.log(schema1);
+    // console.log(schema);
+    const uiSchema = require("../../jsonFiles/uiSchema.json");
+    // const CustomSchemaField = function () {
+    //   return (
+    //     <div id="custom">
+    //       <p>Yeah, I'm pretty dumb.</p>
+    //       {/* <SchemaField {...props} /> */}
+    //     </div>
+    //   );
+    // };
+    // const CustomTitleField = ({ title, required }) => {
+    //   //const legend = required ? title + "****" : title;
+    //   return <div id="custom">legend</div>;
+    // };
+    // const fields = {
+    //   TitleField: CustomTitleField,
+    // };
+    // const handleSubmit = () => {
+    //   console.log(formData);
+    // };
+    return (
+        <div>
+            <div className="form">
+                <Form
+                    schema={schema1}
+                    uiSchema={uiSchema}
+                    // formData={formData}
+                    // onChange={(e) => {
+                    //   setformData(e.formData);
+                    //   console.log("Hii");
+                    //   console.log(formData);
+                    // }}
+                    onSubmit={handleSubmit}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default FormComponent;
--- Frontend/src/components/GridComponent.js
+++ Frontend/src/components/GridComponent.js
@@ -1,101 +1,54 @@
 import Form from "@rjsf/core";
-
 import React, { useEffect, useState } from "react";
-
 import axios from "axios";
-
 import "./GridComponent.css";
 import { Edit } from "@mui/icons-material";
 
 const GridComponent = (lowercaseAnnotation) => {
   const [formData, setFormData] = useState([]);
-
   const [searchGrid, setSearchGrid] = useState([]);
-
   const [getAPIData, setAPIData] = useState([]);
-
   const uischema = require("../jsonFiles/uiSchema.json");
-
   const searchBox = require("../jsonFiles/searchBox.json");
-const [showformbutton, setshowformbutton] = useState(false)
+  const [showformbutton, setshowformbutton] = useState(false)
 
   const handleSubmit = () => {
-    console.log(formData);
-
-    console.log(formData.Search);
-
-    console.log(formData.Comment);
-
     const valueGridData = searchBox.custom.split(",");
-
     const valueCustomGridData = valueGridData[0].split("'");
-
     const getSearchData = valueCustomGridData[1] + formData.Search;
-
-    console.log(searchBox.properties.Search.key);
-
-    console.log(getSearchData);
-
+   
     if (formData.Search) {
       axios
-
         .get(getSearchData)
-
         .then((res) => {
           if (res && res.data) {
             setSearchGrid(res.data);
-
             console.log(res.data);
-
             setAPIData(res.data);
-
-            // console.log(formData.Search);
-
-            console.log(searchGrid);
-
             return res.data;
           } else return [];
         })
-
         .catch((error) => {
           return error;
         });
     }
   };
 
-  console.log(searchGrid);
-
-  console.log(getAPIData);
-
   const [newApiState, setNewApiState] = useState([]);
 
   const getGridData = () => {
     axios
-
       .get(`https://localhost:7184/api/Customers/Customers`)
-
       .then((res) => {
         if (res && res.data) {
-          // props = res.data;
-
-          console.log(res.data);
-
           setAPIData(res.data);
-
           setNewApiState(res.data);
-
-          // return props;
         } else return [];
       })
-
       .catch((error) => {
         return error;
       });
-
-    //   return response;
   };
-
-  // getGridData();
 
   const handleEditClick = (apiGridItems) => {
     localStorage.setItem("editData", JSON.stringify(apiGridItems));
@@ -105,25 +58,13 @@ const [showformbutton, setshowformbutton] = useState(false)
     !newApiState.length && getGridData();
   }, []);
 
-  console.log(newApiState);
-
-  console.log(getAPIData);
-
-  // const fields = {
-
-  //   SchemaField: CustomSchemaField,
-
-  // };
-  console.log(lowercaseAnnotation);
   const annotation=lowercaseAnnotation.lowercaseAnnotation;
-  console.log(annotation);
+
 useEffect(() => {
   if(annotation.includes("update")){
     setshowformbutton(true)
   }
 }, [lowercaseAnnotation])
-
-console.log(showformbutton);
 
   return (
     <div>
@@ -135,10 +76,6 @@ console.log(showformbutton);
           {getAPIData &&
             getAPIData.length > 0 &&
             Object.keys(getAPIData[0]).map((key, id) => {
-              console.log(Object.keys(getAPIData[0]));
-
-              console.log(key);
-
               return (         
                 <div className={"gridColumnHeadingItem"} htmlFor={id}>
                   {key}
@@ -146,13 +83,9 @@ console.log(showformbutton);
               );
             })}
         </div>
-
-        <div className={"gridDataAPI"}>
-      
+        <div className={"gridDataAPI"}>    
           {getAPIData.length > 0 &&
             Object.keys(getAPIData).map((key, index) => {
-              console.log(getAPIData);
-
               return (
                 <div className={"apiGridRow"} key={index}>
                     <div className={"apiGridItems"}>
@@ -176,15 +109,9 @@ console.log(showformbutton);
         schema={searchBox}
         uiSchema={uischema}
         formData={formData}
-        // fields={fields}
-
         idPrefix={"rjsf_prefix"}
         onChange={(e) => {
           setFormData(e.formData);
-
-          console.log(formData);
-
-          console.log(formData.Search);
         }}
         onSubmit={(e) => handleSubmit()}
       />
