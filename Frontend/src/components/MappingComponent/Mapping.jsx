b36da166218d5f69a846858204caa145c07c249b
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2023-07-14 14:32:23 +0530

    small changes

--- Frontend/src/components/MappingComponent/Mapping.jsx
+++ Frontend/src/components/MappingComponent/Mapping.jsx
@@ -778,15 +778,8 @@ const Mapping = ({ jsonData, setJsonData }) => {
                     </Container>
                 </div>
             )}
-            {!showGridComponent && (
-                <div
-                    className={
-                        lowercaseAnnotation.includes("create")
-                            ? "create"
-                            : "get"
-                    }
-                    ref={formRef}
-                >
+            {!showGridComponent && lowercaseAnnotation.includes("get") && (
+                <div className={"get"} ref={formRef}>
                     <Container
                         // style={{ background: '#f3e5f5' }}
                         // {annotation.includes("create")?className="create" : className="get" }
